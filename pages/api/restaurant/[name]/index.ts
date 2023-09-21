ccba3786faf8e547aa59e4b823f90989f2340307
kelvinwongdev <wonglongchun@gmail.com>
2023-09-21 17:08:53 +0100

    updated data format and fix the TS issues

--- pages/api/restaurant/[name]/index.ts
+++ pages/api/restaurant/[name]/index.ts
@@ -1,22 +1,36 @@
 import type { NextApiResponse, NextApiRequest } from "next";
 import restautants from "@/data/restaurants.json";
 import restautantsMenus from "@/data/restaurants-menus.json";
+import { MenuType } from "@/components/menu/Menu";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const name = Array.isArray(req.query.value)
     ? req.query.value[0]
     : req.query.value;
 
-  const data = {
+  // const menus: MenuType[] = restautantsMenus[name] as MenuType[];
+
+  type Data = {
+    title: string;
+    imageSrc: string;
+    menus?: any[];
+  };
+
+  const data: Data = {
     title: "",
     imageSrc: "",
-    menus: restautantsMenus[name],
   };
 
   restautants.map((restaurant) => {
     if (restaurant.name === name && !!name) {
       data.title = restaurant.title;
       data.imageSrc = restaurant.imageSrc;
+    }
+  });
+
+  restautantsMenus.map((restaurant) => {
+    if (restaurant.name === name && !!name) {
+      data.menus = new Array(restaurant.menus);
     }
   });
 
